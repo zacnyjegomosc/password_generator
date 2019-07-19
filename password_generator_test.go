5d9d1822776e1af3f3f45fe6776e65a84d5f22cb
Marek Glijer <mglijer@gmail.com>
2019-07-20 00:48:34 +0200

    [mg/1] Adding a new test for dummy-checking a characters distribution.

--- password_generator_test.go
+++ password_generator_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"sort"
+	"strconv"
+	"testing"
+)
 
 func TestGenerateCryptoSafeStringShortLen(t *testing.T) {
 	const desiredLenOfString = 1
@@ -51,5 +55,78 @@ func TestGenerateCryptoSafeStringDummyCollision(t *testing.T) {
 		hashMapOfRandomStrings[cryptoSafeString] = defaultValueForStringInHashMap
 
 		i += 1
+	}
+}
+
+func TestGenerateCryptoSafeStringCharsDistribution(t *testing.T) {
+	const (
+		maxTestCount = 100  // We want to make this test X times
+		checkFirstCharsCount = 8  // We want to check a first X chars from our score list.
+		digitIsTheBeginningThreshold = 50  // We want to have at least X digits occurs.
+		desiredLenOfString = 32  // Our len of generated random string.
+		maxIterations = 10000  // We want to generate X string for one test.
+	)
+
+	testCounter := 0
+	digitAtTheBeginningOfStringCount := 0
+	allTriesCounter := 0
+
+	// For every test...
+	for testCounter < maxTestCount {
+		i := 0
+		// Declare a hash map for chars
+		hashMapOfChars := map[string]int{}
+
+		for i < maxIterations {
+			// Generate a new random string
+			var cryptoSafeString = GenerateCryptoSafeString(desiredLenOfString)
+
+			// Add first char to hash map and increment counter ("char-score")
+			hashMapOfChars[string(cryptoSafeString[0])] += 1
+
+			i += 1
+		}
+
+		// Sort our char-score list (descending), to build a "winner list"
+		numbersMap := map[int][]string{}
+		var sliceToSort []int
+		for letterKey, letter := range hashMapOfChars {
+			numbersMap[letter] = append(numbersMap[letter], letterKey)
+		}
+		for k := range numbersMap {
+			sliceToSort = append(sliceToSort, k)
+		}
+		sort.Sort(sort.Reverse(sort.IntSlice(sliceToSort)))
+
+		// Check all the "winners"
+		charIteration := 1
+		for _, k := range sliceToSort {
+			allTriesCounter += 1
+
+			// When we check all winners - break the loop and don't check the losers
+			if charIteration >= checkFirstCharsCount {
+				break
+			}
+
+			// When this "winning" char is a digit...
+			for _, char := range numbersMap[k] {
+
+				if _, err := strconv.Atoi(char); err == nil {
+					// Increment a count of digits in "winners list"
+					digitAtTheBeginningOfStringCount += 1
+				}
+			}
+
+			charIteration += 1
+
+		}
+		testCounter += 1
+	}
+
+	// If we have to much digits in "winners"...
+	if digitAtTheBeginningOfStringCount < digitIsTheBeginningThreshold {
+		// This is bad.
+		t.Errorf("We have a small chance to get a digit in the beginning. Now it was the only %d for %d tries.",
+			digitAtTheBeginningOfStringCount, allTriesCounter)
 	}
 }
